b726acf2d062cbbecbd0a6eb9e9d91c61a024f02
Umer Moazzam <click.umer50@gmail.com>
2025-12-30 02:00:12 +0500

    Updated Lack of Splash Screen UI

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'login_screen.dart'; 
+import 'login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,16 +9,48 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
   @override
   void initState() {
     super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _controller.forward();
+
     Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const LoginScreen()),
       );
     });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _buildFadeIn(Widget child, int delayMilliseconds) {
+    return FadeTransition(
+      opacity: Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(
+          parent: _controller,
+          curve: Interval(
+            delayMilliseconds / 2500,
+            1.0,
+            curve: Curves.easeIn,
+          ),
+        ),
+      ),
+      child: child,
+    );
   }
 
   @override
@@ -30,56 +62,38 @@ class _SplashScreenState extends State<SplashScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TweenAnimationBuilder<double>(
-                tween: Tween(begin: 0.0, end: 1.0),
-                duration: const Duration(milliseconds: 1200),
-                builder: (context, value, child) {
-                  return Opacity(
-                    opacity: value,
-                    child: Text(
-                      'Blood Care',
-                      style: GoogleFonts.poppins(
-                        fontSize: 40,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black87,
-                      ),
-                    ),
-                  );
-                },
+              _buildFadeIn(
+                Text(
+                  'Blood Care',
+                  style: GoogleFonts.poppins(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black87,
+                  ),
+                ),
+                0,
               ),
               const SizedBox(height: 40),
-              TweenAnimationBuilder<double>(
-                tween: Tween(begin: 0.0, end: 1.0),
-                duration: const Duration(milliseconds: 1200),
-                builder: (context, value, child) {
-                  return Opacity(
-                    opacity: value,
-                    child: Image.asset(
-                      'assets/blood_care.png',
-                      width: 130,
-                      height: 130,
-                      fit: BoxFit.contain,
-                    ),
-                  );
-                },
+              _buildFadeIn(
+                Image.asset(
+                  'assets/blood_care.png',
+                  width: 130,
+                  height: 130,
+                  fit: BoxFit.contain,
+                ),
+                400,
               ),
               const SizedBox(height: 40),
-              TweenAnimationBuilder<double>(
-                tween: Tween(begin: 0.0, end: 1.0),
-                duration: const Duration(milliseconds: 1200),
-                builder: (context, value, child) {
-                  return Opacity(
-                    opacity: value,
-                    child: Text(
-                      'Blood Donation Mobile App',
-                      style: GoogleFonts.poppins(
-                        fontSize: 17,
-                        fontWeight: FontWeight.w500,
-                        color: const Color(0xFF757575),
-                      ),
-                    ),
-                  );
-                },
+              _buildFadeIn(
+                Text(
+                  'Blood Donation Mobile App',
+                  style: GoogleFonts.poppins(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                    color: const Color(0xFF757575),
+                  ),
+                ),
+                800,
               ),
             ],
           ),
